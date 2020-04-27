c80a25b26437fe77948a3bde8539df538f6e245f
MeitarFuchs <meitardar2@gmail.com>
2020-04-27 19:32:51 +0300

    Part A

---
+++ cmake-build-debug/CMakeFiles/solverEx3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solverEx3.dir/main.cpp.o"
+  "solverEx3"
+  "solverEx3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solverEx3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/solverEx3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for solverEx3.
+# This may be replaced when dependencies are built.
